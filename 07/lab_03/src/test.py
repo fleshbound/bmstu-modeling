f82a7ee69dbc8c524334a0145626343399d0320f
Valeria Avdeykina <117504017+fleshbound@users.noreply.github.com>
2025-01-19 20:08:26 +0300

    delete 07 dir

--- 07/lab_03/src/test.py
+++
@@ -1,7 +0,0 @@
-from twist_generator import TwistGenerator
-
-gen = TwistGenerator()
-gen.SetW(4)
-gen.start(2)
-for i in range(40):
-    print(gen.Next())
--- 07/lab_04/src/color.py
+++
@@ -1,5 +0,0 @@
-PURPLE_SUPER_DARK  = "#4d293d"
-PURPLE_DARK        = "#a375a3"
-PURPLE             = "#ccb3cc"
-PURPLE_LIGHT       = "#ebb3e0"
-PURPLE_SUPER_LIGHT = "#f5f0f5"
